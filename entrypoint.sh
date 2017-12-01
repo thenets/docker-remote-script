8f2a94fe8c4141ddfd2d1dd5ab6ffb42a2a793a0
Luiz Felipe F M Costa <luiz@thenets.org>
2017-11-30 23:30:53 -0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+# Check if $SCRIPT_PATH exists
+if [[ -z "${SCRIPT_PATH}" ]]; then
+  echo "The SCRIPT_PATH doesn't exist! Please set this environment variable."
+else
+  # Start script
+  curl -sSL $SCRIPT_PATH | sh
+fi
+
+echo "... done!"
